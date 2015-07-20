be118c2578b150ffda24112de2af63442cb77ce9
Brian Blosser <bpb54321@gmail.com>
2015-07-20 09:43:24 -0500

    Coin calculator passes all tests, but does not yet interface with user via console

--- src/main/java/CoinCalculator.java
+++ src/main/java/CoinCalculator.java
@@ -1,19 +1,43 @@
 /**
- * User: mpmenne
- * Date: 6/18/14
- * Time: 3:06 AM
+ * User: Brian Blosser
+ * Date: 7/19/15
+ * Time: 8:31 pm
  */
 public class CoinCalculator {
 
 
     public String calculateChange(String amountOfChange) {
-        Integer change = Integer.parseInt(amountOfChange.substring(amountOfChange.length() - 2));
-        if(change > 9) {
-            return "1 dime";
-        } else if (change > 0) {
-            return "1 penny";
+        int change = Integer.parseInt(amountOfChange.substring(amountOfChange.length() - 2));
+        int numOfCoins;
+        String coinMessage="";
+        int[] denoms={25,10,5,1};
+        String[] denomStringsSingular={"quarter", "dime", "nickel", "penny"};
+        String[] denomStringsPlural={"quarters", "dimes", "nickels", "pennies"};//Dealing with plural spelling.
+
+        if (change>0) {
+            for (int i = 0; i < denoms.length; i++) {
+
+                numOfCoins = change / denoms[i];
+
+                if (numOfCoins > 1) {
+                    coinMessage += numOfCoins + " " + denomStringsPlural[i] + ", ";
+                } else if (numOfCoins==1) {
+                    coinMessage += numOfCoins + " " + denomStringsSingular[i] + ", ";
+                } else {
+                    //Do not add anything to coinMessage.
+                }
+                change = change % denoms[i];
+                if (change==0) { //coins go in evenly
+                    break;
+                }
+            }
+            coinMessage=coinMessage.substring(0,coinMessage.length()-2); //Trim extra comma and space at end.
+
         } else {
-            return "No coins returned";
+            coinMessage = "No coins returned";
         }
+
+        return coinMessage;
+        
     }
 }
--- src/test/java/CoinCalculatorTest.java
+++ src/test/java/CoinCalculatorTest.java
@@ -10,12 +10,76 @@ import static org.junit.Assert.assertEquals;
 public class CoinCalculatorTest {
 
     @Test
-    public void zeroChangeMeansYouGetZeroCoins() {
+    public void testOfNinetyNineCents() {
         CoinCalculator coinCalculator = new CoinCalculator();
 
-        String coinMessage = coinCalculator.calculateChange("$3.00");
+        String coinMessage = coinCalculator.calculateChange("$0.99");
 
-        assertEquals("No coins returned", coinMessage);
+        assertEquals("3 quarters, 2 dimes, 4 pennies", coinMessage);
+    }
+
+    @Test
+    public void test95Cents() {
+        CoinCalculator coinCalculator = new CoinCalculator();
+
+        String coinMessage = coinCalculator.calculateChange("$0.95");
+
+        assertEquals("3 quarters, 2 dimes", coinMessage);
+    }
+
+    @Test
+    public void test91Cents() {
+        CoinCalculator coinCalculator = new CoinCalculator();
+
+        String coinMessage = coinCalculator.calculateChange("$0.91");
+
+        assertEquals("3 quarters, 1 dime, 1 nickel, 1 penny", coinMessage);
+    }
+
+    @Test
+    public void test90Cents() {
+        CoinCalculator coinCalculator = new CoinCalculator();
+
+        String coinMessage = coinCalculator.calculateChange("$0.90");
+
+        assertEquals("3 quarters, 1 dime, 1 nickel", coinMessage);
+    }
+
+    @Test
+    public void test81Cents() {
+        CoinCalculator coinCalculator = new CoinCalculator();
+
+        String coinMessage = coinCalculator.calculateChange("$0.81");
+
+        assertEquals("3 quarters, 1 nickel, 1 penny", coinMessage);
+    }
+
+    @Test
+    public void test80Cents() {
+        CoinCalculator coinCalculator = new CoinCalculator();
+
+        String coinMessage = coinCalculator.calculateChange("$0.80");
+
+        assertEquals("3 quarters, 1 nickel", coinMessage);
+    }
+
+
+    @Test
+    public void test50CentsShouldGiveTwoQuarters() {
+        CoinCalculator coinCalculator = new CoinCalculator();
+
+        String coinMessage = coinCalculator.calculateChange("$0.50");
+
+        assertEquals("2 quarters", coinMessage);
+    }
+
+    @Test
+    public void tenCentsShouldGiveYouOneDime() {
+        CoinCalculator coinCalculator = new CoinCalculator();
+
+        String coinMessage = coinCalculator.calculateChange("$0.10");
+
+        assertEquals("1 dime", coinMessage);
     }
 
     @Test
@@ -28,12 +92,20 @@ public class CoinCalculatorTest {
     }
 
     @Test
-    public void tenCentsShouldGiveYouOneDime() {
+    public void zeroChangeMeansYouGetZeroCoins() {
         CoinCalculator coinCalculator = new CoinCalculator();
 
-        String coinMessage = coinCalculator.calculateChange("$0.10");
+        String coinMessage = coinCalculator.calculateChange("$3.00");
 
-        assertEquals("1 dime", coinMessage);
+        assertEquals("No coins returned", coinMessage);
     }
+
+
+
+
+
+
+
+
 
 }
